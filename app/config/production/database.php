0b6d5dfefb45109701686060b7f40c1c3eb3bb85
Volodymyr Yatsevsky <yatsevsky@gmail.com>
2015-05-11 22:16:04 +0300

    Fix config, oops

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -21,11 +21,14 @@ return array(
 
 	'default' => 'sqlite',
 
-	'connections' => array(
-		'sqlite' => array(
-<------><------><------>'driver'   => 'sqlite',
-<------><------><------>'database' => __DIR__.'/../database/production.sqlite',
-<------><------><------>'prefix'   => '',
-<------><------>),
-	),
+    'connections' => array(
+
+        'sqlite' => array(
+            'driver'   => 'sqlite',
+            'database' => __DIR__.'/../../database/production.sqlite',
+            'prefix'   => '',
+        ),
+
+    ),
+    
 );
